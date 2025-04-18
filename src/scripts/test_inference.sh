61f9f29f0ef16fcb881e7c45b4d443c602eeaeaf
ueycz@student.kit.edu <ka_ueycz@uc3n005.localdomain>
2025-04-18 10:07:09 +0200

    Set up directory structure for packaging

---
+++ src/scripts/test_inference.sh
@@ -0,0 +1,17 @@
+cd "$(dirname "$0")"
+cd ../
+
+export PYTHONPATH=$PYTHONPATH:$PWD
+export CUDA_VISIBLE_DEVICES=0
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+    export PHONEMIZER_ESPEAK_LIBRARY=/opt/homebrew/Cellar/espeak-ng/1.52.0/lib/libespeak-ng.dylib
+fi
+
+python3 infer/infer.py \
+    --lrc-path infer/example/eg_en_full.lrc \
+    --ref-audio-path infer/example/eg_en.mp3 \
+    --audio-length 95 \
+    --repo_id ASLP-lab/DiffRhythm-full \
+    --output-dir infer/example/output \
+    --chunked
